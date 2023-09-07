6986704d2145e7f2a34fbbc6288d6fc4c4c9225c
jeff <jeff.etale@student.moringaschool.com>
2023-09-07 12:51:15 +0300

    fixed an error that failed to import models module to config module

--- configuration/config.py
+++ configuration/config.py
@@ -1,9 +1,19 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model.models import Base
 
-DATABASE_URI = 'sqlite:///..finntasker.db'
+DATABASE_URI = 'sqlite:///finntasker.db'
 
 engine = create_engine(DATABASE_URI)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+Base.metadata.create_all(engine)
